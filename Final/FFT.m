a3c83929d3e228214658cf9c5c8af81bdd786a1c
Sevi-S <44700142+Sevi-S@users.noreply.github.com>
2019-01-23 11:44:34 +0100

    Update FFT.m

--- Final/FFT.m
+++ Final/FFT.m
@@ -1,65 +1,52 @@
 %% STFT on HFO candidate
+%fast fourier transform
 
-Mario4_GMM_Table = Mario4_Filtered_8min_HilbertCrossing;
+Mario4_GMM_Table = Mario4_Filtered_8min_HilbertCrossing; %load data
 Matrix=[];
 count=1;
-for ii= 1:length(Mario4_Filtered_8min_HilbertCrossing.ChNum)
+for ii= 1:length(Mario4_Filtered_8min_HilbertCrossing.ChNum)    %length
 
-
-row=Mario4_Filtered_8min_HilbertCrossing.ChNum(ii);
-center_peak=round((Mario4_Filtered_8min_HilbertCrossing.startsegment(ii)+Mario4_Filtered_8min_HilbertCrossing.endsegment(ii))/2);
-colstar=center_peak-266;
-colend=center_peak+266;
-L=length(colstar:colend);
+    row=Mario4_Filtered_8min_HilbertCrossing.ChNum(ii);
+    center_peak=round((Mario4_Filtered_8min_HilbertCrossing.startsegment(ii)+Mario4_Filtered_8min_HilbertCrossing.endsegment(ii))/2);
+    colstar=center_peak-266;
+    colend=center_peak+266;     %peaks on either side, 128ms on each side so 266 data points
+    L=length(colstar:colend);
     
+    Y=fft(Mario4raw_8min(row,colstar:colend));      %Matlab fft
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
 
+    f = 2048*(0:(L/2))/L;
 
-Y=fft(Mario4raw_8min(row,colstar:colend));
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
+    total_power=sum(P1);%get power
 
-
-
-
-f = 2048*(0:(L/2))/L;
-
-total_power=sum(P1);
-
-for i = 1:length(P1)
+        for i = 1:length(P1) 
     
-    if P1(1,i)< 0.01*total_power
-       P1(1,i)=0;
-    end
+            if P1(1,i)< 0.01*total_power
+                 P1(1,i)=0;
+            end
     
-end
+        end
 
-Non_zero=find(P1);
+ Non_zero=find(P1); % find zeros
 
- for i=length(Non_zero)
+    for i=length(Non_zero)
+ 
+         if f(Non_zero(i))> 80 
+          
+         else
+    
+        fprintf('This rows should be deleted: %d .\n', ii)
      
-    if f(Non_zero(i))> 80 
-        
+        Matrix(0+count,1)=ii;
        
-       
-        
-     else
-    
-       fprintf('This rows should be deleted: %d .\n', ii)
+        count=count+1;
       
-       
-       Matrix(0+count,1)=ii;
-       
-       count=count+1;
-       
-       
-           
-      
+        end
      end
-      
- end
+ 
 end
-
 
 Mario4_GMM_Table(Matrix,:) = [];
 
