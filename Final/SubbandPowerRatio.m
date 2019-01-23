a63e5c7cf3449e1fe873e072f9eee1411b3a5f33
Sevi-S <44700142+Sevi-S@users.noreply.github.com>
2019-01-23 11:54:04 +0100

    Update SubbandPowerRatio.m

--- Final/SubbandPowerRatio.m
+++ Final/SubbandPowerRatio.m
@@ -1,6 +1,6 @@
 %% Subband_power_ratio
 
-for ii= 1:length(Mario4_GMM_Table.ChNum)
+for ii= 1:length(Mario4_GMM_Table.ChNum)%scroll through data
     
      
 row=Mario4_GMM_Table.ChNum(ii);
@@ -9,26 +9,26 @@ row=Mario4_GMM_Table.ChNum(ii);
 windowlength=ceil((Mario4_GMM_Table.lengthsegment(1)*0.48)/2);
 overlaplength=ceil(windowlength/4);
     
-
+%power
 [pxx,f] = pwelch(data(row,Mario4_GMM_Table.startsegment(ii):Mario4_GMM_Table.endsegment(ii)),windowlength,overlaplength,1024,2048);
 
 
-low_band = f > 16 & f < 80;
+low_band = f > 16 & f < 80;%find low band
 low_band_indices=find(low_band == max(low_band(:)));
 
-high_band = f> 80 & f < 500;
+high_band = f> 80 & f < 500;%find highband
 high_band_indices=find(high_band == max(high_band(:)));
 
 
 
 
-ratio = sum(pxx(high_band_indices(1):high_band_indices(end)))/sum(pxx(low_band_indices(1):low_band_indices(end)));
+ratio = sum(pxx(high_band_indices(1):high_band_indices(end)))/sum(pxx(low_band_indices(1):low_band_indices(end)));%calculate ratio
 
-Mario4_GMM_Table.Power_ratio(ii)={ratio};
+Mario4_GMM_Table.Power_ratio(ii)={ratio};%push to table
 
 
 
 end
 
- Mario4_GMM_Table.Power_ratio=cell2mat(Mario4_GMM_Table.Power_ratio);
+ Mario4_GMM_Table.Power_ratio=cell2mat(Mario4_GMM_Table.Power_ratio);%cell to number
 
