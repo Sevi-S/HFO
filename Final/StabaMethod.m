04e960fdc313ace2614b7c8fb1235c513e08f73f
Sevi-S <44700142+Sevi-S@users.noreply.github.com>
2019-01-24 10:37:50 +0100

    Update StabaMethod.m

--- Final/StabaMethod.m
+++ Final/StabaMethod.m
@@ -3,20 +3,20 @@ count = 1;
 
 Table = {0,0,0,0};
 
-while length(data.Mario3filtered_8min(:,1)) >= count
+while length(data.Mario3filtered_8min(:,1)) >= count %scroll through data 1 channel at a time
 
     y = data.Mario3filtered_8min(count,:);
     
     k = 205;  %%100ms window at 2048hz sample rate
-    M = movstd(y,k);
-    med = median(M);
-    ampThresh = 5*med;
+    M = movstd(y,k);    %moving standard deviation
+    med = median(M);    %mean   
+    ampThresh = 5*med;  % define threshold
     
     %analyze one channel
     %will get put into a table in getChan
     
-    t = getHFO(y,ampThresh,count);
-    Table = [Table;t];
+    t = getHFO(y,ampThresh,count);  %get hfos
+    Table = [Table;t]; %export to table
     
     count = count + 1;
 end
